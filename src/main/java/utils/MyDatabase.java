ef3bb1243b285d6bb39bcc3b2df127b47e5b23d6
Jevro1337 <malekamir56@gmail.com>
2024-02-09 19:14:50 +0100

    Updated DB Name

--- src/main/java/utils/MyDatabase.java
+++ src/main/java/utils/MyDatabase.java
@@ -32,34 +32,4 @@ public class MyDatabase {
         return connection;
     }
 
-    /*
-    private final String URL = "jdbc:mysql://localhost:3306/projet_pi";
-    private final String USER = "root";
-    private final String PASS = "";
-    private Connection connection;
-    private static MyDatabase instance;
-
-    public MyDatabase() {
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASS);
-            System.out.println("Connected to the database");
-        } catch (SQLException e) {
-            System.err.println(e.getMessage());
-            System.exit(1);
-        }
-    }
-
-    public static MyDatabase getInstance() {
-        if (instance == null)
-            instance = new MyDatabase();
-        else
-            System.out.println("Database already connected");
-        return instance;
-    }
-
-    public Connection getConnection() {
-        return connection;
-    }
-
-     */
 }

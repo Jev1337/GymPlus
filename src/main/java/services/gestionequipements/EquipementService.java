2d22e47db54e61e7fe4c76e28b82ccfe94f7fb77
Jevro1337 <malekamir56@gmail.com>
2024-02-09 19:10:50 +0100

    Initial CRUD Commit
    Co-Authored-By: yasmine1337 <101060065+yasmine1337@users.noreply.github.com>

---
+++ src/main/java/services/gestionequipements/EquipementService.java
@@ -0,0 +1,64 @@
+package services.gestionequipements;
+
+import entities.gestionequipements.Equipements_details;
+import utils.MyDatabase;
+import services.IService;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class EquipementService implements IService<Equipements_details>{
+
+    private Connection connection;
+
+    public EquipementService() {
+        connection= MyDatabase.getInstance().getConnection();
+    }
+
+    @Override
+    public void add(Equipements_details equipementsDetails) throws SQLException {
+        String sql="insert into equipements_details values(?,?,?,?,?)";
+        PreparedStatement ps =connection.prepareStatement(sql);
+        ps.setInt(1,equipementsDetails.getId());
+        ps.setString(2, equipementsDetails.getName());
+        ps.setString(3,equipementsDetails.getDescription());
+        ps.setString(4,equipementsDetails.getDuree_de_vie());
+        ps.setString(5,equipementsDetails.getEtat());
+        ps.executeUpdate();
+    }
+
+    @Override
+    public void update(Equipements_details equipementsDetails) throws SQLException
+    {
+        String sql="update equipements_details set name=?, description=? ,duree_de_vie=?, etat=? where id=?";
+        PreparedStatement ps =connection.prepareStatement(sql);
+        ps.setString(1, equipementsDetails.getName());
+        ps.setString(2,equipementsDetails.getDescription());
+        ps.setString(3,equipementsDetails.getDuree_de_vie());
+        ps.setString(4,equipementsDetails.getEtat());
+        ps.setInt(5,equipementsDetails.getId());
+        ps.executeUpdate();
+    }
+
+    @Override
+    public void delete(int id) throws SQLException {
+        String sql="delete from equipements_details where id =?";
+        PreparedStatement ps =connection.prepareStatement(sql);
+        ps.setInt(1, id);
+        ps.executeUpdate();
+    }
+
+    @Override
+    public List<Equipements_details> getAll() throws SQLException {
+        String sql = "select * from equipements_details";
+        Statement st = connection.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        List<Equipements_details> equipements = new ArrayList<>();
+        while (rs.next()) {
+            equipements.add(new Equipements_details(rs.getInt("id"),rs.getString("name"),rs.getString("description"),rs.getString("duree_de_vie"),rs.getString("etat")));
+        }
+        return equipements;
+
+    }
+}

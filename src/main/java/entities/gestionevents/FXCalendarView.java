c2b3ba72e0775d7696c79a5cba3be4f5562cba20
mohamed farouk chbichib <100930477+Kayamii@users.noreply.github.com>
2024-02-20 18:35:41 +0100

    edited points remaining

---
+++ src/main/java/entities/gestionevents/FXCalendarView.java
@@ -0,0 +1,10 @@
+package entities.gestionevents;
+
+import com.calendarfx.view.CalendarView;
+
+public class FXCalendarView extends CalendarView {
+    public FXCalendarView() {
+        super();
+    }
+
+}

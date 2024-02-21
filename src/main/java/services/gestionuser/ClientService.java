a5d5efd9c72824bc604684b8bd116ac6af0d677c
mohamed farouk chbichib <100930477+Kayamii@users.noreply.github.com>
2024-02-21 13:59:36 +0100

    calendar removed

--- src/main/java/services/gestionuser/ClientService.java
+++ src/main/java/services/gestionuser/ClientService.java
@@ -178,7 +178,8 @@ public class ClientService implements IService<Client> {
         }
 
         return 0;}
-    public List<Client> getNonSubscribedUserList() throws SQLException {
+
+        public List<Client> getNonSubscribedUserList() throws SQLException {
         List<Client> clients = new ArrayList<>();
         String query = "SELECT * FROM user WHERE role = 'client' AND id NOT IN (SELECT user_id FROM abonnement WHERE dateFinAb > NOW())";
         Statement st = connection.createStatement();

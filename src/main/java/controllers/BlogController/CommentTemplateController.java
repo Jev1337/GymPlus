3229efceb6dcf1f7fb1cdd58bbde77e9eba3debe
ilyes <ilyesarous9645@gmail.com>
2024-02-24 12:04:02 +0100

    bad words filter activated + bd updated

--- src/main/java/controllers/BlogController/CommentTemplateController.java
+++ src/main/java/controllers/BlogController/CommentTemplateController.java
@@ -2,12 +2,7 @@ package controllers.BlogController;
 
 import controllers.gestionuser.GlobalVar;
 import entities.gestionblog.Commentaire;
-import entities.gestionblog.Post;
-import javafx.animation.Interpolator;
-import javafx.animation.ScaleTransition;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -15,20 +10,12 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import javafx.util.Duration;
 import services.gestionuser.ClientService;
 import services.gestonblog.CommentaireService;
 
 import java.io.File;
-import java.io.IOException;
-import java.sql.Date;
 import java.sql.SQLException;
-import java.util.Objects;
 
 
 public class CommentTemplateController {
@@ -47,6 +34,7 @@ public class CommentTemplateController {
     private Commentaire commentaire = new Commentaire();
     private final ClientService us = new ClientService();
     private final CommentaireService cs = new CommentaireService();
+
 
     public CommentTemplateController() {
     }
--- src/main/java/entities/gestionblog/Post.java
+++ src/main/java/entities/gestionblog/Post.java
@@ -1,6 +1,6 @@
 package entities.gestionblog;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Post {
 

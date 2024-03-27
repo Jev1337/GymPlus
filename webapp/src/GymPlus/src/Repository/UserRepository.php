606a7a97ec6107f70962bf1e120afd15d214409d
Jevro1337 <malekamir56@gmail.com>
2024-03-27 06:30:23 +0100

    Update UserRepository.php

--- webapp/src/GymPlus/src/Repository/UserRepository.php
+++ webapp/src/GymPlus/src/Repository/UserRepository.php
@@ -20,6 +20,12 @@ class UserRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, User::class);
     }
+    public function showAllUsers()
+    {
+        return $this->createQueryBuilder('u')
+            ->getQuery()
+            ->getResult();
+    }
 
 //    /**
 //     * @return User[] Returns an array of User objects

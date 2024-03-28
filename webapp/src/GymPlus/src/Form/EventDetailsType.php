c24ef39fb4f6892d5bd395ae628a65974ac37416
mohamed farouk chbichib <100930477+Kayamii@users.noreply.github.com>
2024-03-28 19:23:55 +0100

    crud b and f

--- webapp/src/GymPlus/src/Form/EventDetailsType.php
+++ webapp/src/GymPlus/src/Form/EventDetailsType.php
@@ -6,7 +6,10 @@ use App\Entity\EventDetails;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 
 class EventDetailsType extends AbstractType
 {
@@ -17,16 +20,15 @@ class EventDetailsType extends AbstractType
             ->add('type')
             ->add('eventDate')
             ->add('duree')
-            ->add('nbPlaces')
-            ->add('nbTotal')  
-            ->add('save',SubmitType::class)
-        ;
+            ->add('nbPlaces', null, ['disabled' => $options['isEditAction'] ?? false])  // Use the option here
+            ->add('save', SubmitType::class);
     }
-
+    
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => EventDetails::class,
+            'isEditAction' => false,  // Add this line to define the new option
         ]);
     }
 }

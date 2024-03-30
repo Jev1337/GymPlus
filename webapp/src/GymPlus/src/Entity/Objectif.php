7adffc7b1868a4e075e2e701b8f4a3f94ce2f741
Fares <benammar.Fares@esprit.tn>
2024-03-30 02:19:47 +0100

    again

--- webapp/src/GymPlus/src/Entity/Objectif.php
+++ webapp/src/GymPlus/src/Entity/Objectif.php
@@ -35,8 +35,8 @@ class Objectif
     #[ORM\Column]
     private ?string $typeobj;
 
-   /* #[ORM\Column]
-    private ?string $coachNames;*/
+   #[ORM\Column]
+    private ?string $coachNames;
 
     #[ORM\ManyToOne(targetEntity: User::class)]
     #[ORM\JoinColumn(name: "userid", referencedColumnName: "id")]

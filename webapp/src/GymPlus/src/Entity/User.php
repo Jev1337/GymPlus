93ff598b71909be71edf5bbbda14db54ac4f677f
ilyes arous <ilyesarous9645@gamil.com>
2024-04-09 14:50:27 +0100

    comnts + posts are functionnal

--- webapp/src/GymPlus/src/Entity/User.php
+++ webapp/src/GymPlus/src/Entity/User.php
@@ -85,10 +85,18 @@ class User Implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?\DateTime $faceidTs;
 
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Post::class, cascade:["remove", "persist", "merge"], orphanRemoval: true)]
+    private Collection $posts;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: Commentaire::class, cascade:["remove", "persist", "merge"], orphanRemoval: true)]
+    private Collection $comments;
+
     
     public function __construct()
     {
         $this->eventDetails = new \Doctrine\Common\Collections\ArrayCollection();
+        $this->posts = new ArrayCollection();
+        $this->comments = new ArrayCollection();
     }
 
     public function getUserIdentifier(): string
@@ -302,6 +310,66 @@ class User Implements UserInterface, PasswordAuthenticatedUserInterface
     {
         if ($this->eventDetails->removeElement($eventDetail)) {
             $eventDetail->removeUser($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Post>
+     */
+    public function getPosts(): Collection
+    {
+        return $this->posts;
+    }
+
+    public function addPost(Post $post): static
+    {
+        if (!$this->posts->contains($post)) {
+            $this->posts->add($post);
+            $post->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removePost(Post $post): static
+    {
+        if ($this->posts->removeElement($post)) {
+            // set the owning side to null (unless already changed)
+            if ($post->getUser() === $this) {
+                $post->setUser(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Commentaire>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Commentaire $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Commentaire $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getUser() === $this) {
+                $comment->setUser(null);
+            }
         }
 
         return $this;
